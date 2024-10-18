4cc6cfb598204e8b90c4e5978c71808b18d3b3c2
Abdirahman Mohamed <a7mohamed@ucsd.edu>
2024-10-17 19:08:29 -0700

    New Change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import screenshot from './images/Calanka_Soomaaliya.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'> 
+      <h1>
+        <img
+          src={screenshot}
+          alt="flower"
+          className="image"
+        />
+      </h1>
+      <h1>Hello, World!</h1>
+      <p>Welcome to my first React app.</p>
+      <h2>About Me:</h2>
+      <b>this sentence is bold</b>
+      <br></br>
+      <i>this sentence is italicized</i>
+      <ul>
+        <li><b>Coding</b></li>
+        <li><i>Quran</i></li>
+        <li>Reading</li>
+      </ul>
+      <h2>My Skills:</h2>
+      <ol>
+        <li>HTML & CSS</li>
+        <li>JavaScript</li>
+        <li>Python</li>
+      </ol>
+      <h3>My Favorite Website</h3>
+      <a href="https://www.youtube.com/shorts/Oinm9S8V3eU">Visit Example Website</a>
     </div>
   );
 }
